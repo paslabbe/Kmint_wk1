43fceee343c16ac228f3fb76475ce45cfbef1dc4
Lennard Slabbekoorn <lennard12@hotmail.nl>
2018-11-17 14:02:47 +0100

    applicatie werkend

--- kmintapp/src/PriorityQueue.h
+++ kmintapp/src/PriorityQueue.h
@@ -6,6 +6,8 @@ struct PriorityQueue {
 	typedef std::pair<priority_t, T> PQElement;
 	std::priority_queue<PQElement, std::vector<PQElement>,
 	                    std::greater<PQElement>> elements;
+	// using iterator = T *;
+	// using const_iterator = const T *;
 
 	inline bool empty() const {
 		return elements.empty();
@@ -20,4 +22,9 @@ struct PriorityQueue {
 		elements.pop();
 		return best_item;
 	}
+
+	// iterator begin() { return &elements[0]; }
+	// const_iterator cbegin() const { return &elements[0]; }
+	// iterator end() { return &elements[elements.size()]; }
+	// const_iterator cend() const { return &elements[elements.size()]; }
 };
--- kmintapp/src/dijkstra.h
+++ kmintapp/src/dijkstra.h
@@ -1,22 +1,29 @@
 #pragma once
 #include "kmint/map/map.hpp"
-#include <map>
 #include "PriorityQueue.h"
-#include "kmint/play/map.hpp"
+#include <map>
+
+//#include "kmint/play/map.hpp"
 
 class dijkstra
 {
+	kmint::map::map_graph& graph_;
 public:
+	dijkstra(kmint::map::map_graph& graph) : graph_(graph)
+	{
+	}
 
+	PriorityQueue<const kmint::map::map_node*, double> dijkstra_search(
+		const kmint::map::map_node& start, const kmint::map::map_node& goal,
+		std::map<const kmint::map::map_node*, const kmint::map::map_node*>& came_from,
+		std::map<const kmint::map::map_node*, double>& cost_so_far);
 
 private:
-	double heuristic(kmint::math::vector2d& a, kmint::math::vector2d& b);
-	PriorityQueue<kmint::map::map_node*, double> dijkstraSearch(
-		kmint::map::map_node* start, kmint::map::map_node* goal,
-		std::map<kmint::map::map_node*, kmint::map::map_node*>& came_from,
-		std::map<kmint::map::map_node*, double>& cost_so_far);
-	PriorityQueue<kmint::map::map_node*, double> astarsearch(kmint::map::map_node* start, kmint::map::map_node* goal,
-	                                                         std::map<kmint::map::map_node*, kmint::map::map_node*>&
-	                                                         came_from,
-	                                                         std::map<kmint::map::map_node*, double>& cost_so_far);
+	//double heuristic(kmint::math::vector2d& a, kmint::math::vector2d& b);
+
+	/*PriorityQueue<kmint::map::map_node*, double> astarsearch(kmint::map::map_node* start, kmint::map::map_node* goal,
+		std::map<kmint::map::map_node*, kmint::map::map_node*>&
+		came_from,
+		std::map<kmint::map::map_node*, double>& cost_so_far);*/
 };
+
